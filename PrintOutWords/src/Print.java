4c505408d9e8acec5c7485538de907bef42afcef
davidfrieder <davidfrieder@10.0.1.131>
2014-07-08 16:38:09 -0700

    learning how to use methods

---
+++ PrintOutWords/src/Print.java
@@ -0,0 +1,28 @@
+public class Print
+{
+	public static void main(String[] args)
+	{
+		name();
+		System.out.println("dumdeedumdeedum");
+		daaarrgh();
+	}
+
+	public static void name()
+	{
+		int integer = 3;
+		int jar = 4;
+		int answer = 0;
+		answer = jar + integer;
+		System.out.println(answer);
+	}
+
+	public static void daaarrgh()
+	{
+		int staap = 6;
+		int bloh = 78;
+		int deh = 1;
+		int added = 0;
+		added = staap + bloh + deh;
+		System.out.println(added);
+	}
+}
