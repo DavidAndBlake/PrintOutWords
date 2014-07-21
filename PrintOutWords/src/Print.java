89c42ca29031a2089c6c9628fc940b8aecc6a480
davidfrieder <davidfrieder@10.0.1.129>
2014-07-21 16:21:29 -0700

    Had some fun with making the computer say stuff. It will be like siri's younger brother soon.

--- PrintOutWords/src/Print.java
+++ PrintOutWords/src/Print.java
@@ -1,28 +1,54 @@
+import javax.swing.JOptionPane;
+
 public class Print
 {
 	public static void main(String[] args)
 	{
 		name();
 		System.out.println("dumdeedumdeedum");
-		daaarrgh();
+		doingThis();
 	}
 
 	public static void name()
 	{
-		int integer = 3;
-		int jar = 4;
+		int integer = 536493;
+		int jar = 545099851;
 		int answer = 0;
+		String aString = " hello.... Hello?..... HELLO!?";
+		boolean hhhhh = true;
 		answer = jar + integer;
-		System.out.println(answer);
+		if (hhhhh)
+		{
+			for (int i = 0; i < 500; i++)
+			{
+				System.out.println(answer + aString + " " + i);
+			}
+		}
 	}
 
-	public static void daaarrgh()
+	public static void doingThis()
 	{
-		int staap = 6;
-		int bloh = 78;
-		int deh = 1;
-		int added = 0;
-		added = staap + bloh + deh;
+		int thisOne = 6;
+		int thisOtherOne = 78;
+		int namedInt = 1;
+		double added = 0;
+		added = thisOne + thisOtherOne + namedInt;
 		System.out.println(added);
+		JOptionPane.showMessageDialog(null, "If I only had a brain.");
+		String canniputor = JOptionPane
+				.showInputDialog("Can I eat your brain?");
+		System.out.println(canniputor);
+		if (canniputor.equals("Not now."))
+		{
+			JOptionPane.showMessageDialog(null, "When?");
+		}
+		if (canniputor.equals("No"))
+		{
+			JOptionPane.showMessageDialog(null, ":'(");
+		}
+		if (canniputor.equals("I don't have one."))
+		{
+			JOptionPane.showMessageDialog(null, "HAHAHAHAHAHAHAHAHAHA!");
+		}
 	}
 }
